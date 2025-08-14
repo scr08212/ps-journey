4f996a6190528623604a76a42f808c5a342920c0
NKM <77067031+scr08212@users.noreply.github.com>
2025-08-14 22:51:59 +0900

    [level 2] Title: 땅따먹기, Time: 6.48 ms, Memory: 25.8 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/README.md
+++ 프로그래머스/2/12913. 땅따먹기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 땅따먹기 - 12913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12913#) 
 
 ### 성능 요약
 
-메모리: 25.9 MB, 시간: 7.72 ms
+메모리: 25.8 MB, 시간: 6.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 14일 22:45:02
+2025년 08월 14일 22:51:56
 
 ### 문제 설명
 
--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -1,40 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 int solution(vector<vector<int> > land)
 {
     int answer = 0;
-    int dp[2][4]{0};
-    for(int i = 0; i<4; i++)
+    int n = land.size();
+    for(int i = 1; i<n; i++)
     {
-        dp[0][i] = land[0][i];
-    }
-    // 점화식: dp[i][j] = land[i][j] + max(dp[i-1][k]) k!=j
-    for(int i = 1; i<land.size(); i++)
-    {
-        for(int j = 0; j<4; j++)
-        {
-            int prev_max = 0;
-            for(int k = 0; k<4; k++)
-            {
-                if(j != k)
-                {
-                    prev_max = max(prev_max, dp[0][k]);
-                }
-            }
-            dp[1][j] = land[i][j] + prev_max;
-        }
-        
-        for(int j = 0; j<4; j++)
-        {
-            dp[0][j] = dp[1][j];
-        }
+        land[i][0] += max(land[i-1][1], max(land[i-1][2], land[i-1][3]));
+        land[i][1] += max(land[i-1][0], max(land[i-1][2], land[i-1][3]));
+        land[i][2] += max(land[i-1][0], max(land[i-1][1], land[i-1][3]));
+        land[i][3] += max(land[i-1][0], max(land[i-1][1], land[i-1][2]));
     }
     
-    for(int i = 0; i<4; i++)
-    {
-        answer = max(answer, dp[0][i]);
-    }
+    answer = *max_element(land[n-1].begin(), land[n-1].end());
+    
     return answer;
 }
